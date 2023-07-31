b462df1aff572a7810bdaf877eb17180acfe2627
Ramirez Isaias Mauro <mauroamaya58@gmail.com>
2023-07-31 17:49:38 -0300

    Añadi un prompt para preguntar que deseara el usuario y un switch que se encargara de realizar la operacion deseada por el usuario

--- js/prueba1.js
+++ js/prueba1.js
@@ -1,13 +1,29 @@
-let nombre; //Variable global, para saber como se llama el usuario
-function saludo() {//Unsaludo  y consigo el nombre del usuario.
 
-    do {
-        nombre=prompt("Ingresa tu Nombre:");
-    } while ((!isNaN(nombre)));
-    alert(`Bienvenido ${nombre}`);
-
+function saludo() {//Un saludo  y consigo el nombre del usuario.
+    let n;
+    do {//Aqui verifico que ingrese un string, y que no entre vacio ni solamente espacios.
+        n=prompt("Ingresa tu Nombre:");
+    } while ((!isNaN(n)));
+    alert(`Bienvenido ${n}`);//Doy el saludo
+    return n;
 }
 
-saludo();
+let nom=saludo();//Llamo la funcion
+
+let op;
+do {
+    op=prompt(`Que operacion desea realizar el dia de hoy?${n} \n1: \n2: \n3:`);
+} while ((op<=0||op>4)||(isNaN(op)));
 
 
+switch (op) {
+    case 1:
+        let mon=prompt('Ingrese la cantidad de pesos que se transformaran a dolares blue.');
+        break;
+    case 2:
+        
+        break;
+    case 1:
+        
+        break;
+}
